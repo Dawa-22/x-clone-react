321e7d600f23741419c4d08e942c9f49ceef8607
Dawit <116512754+Dawa-22@users.noreply.github.com>
2024-11-12 18:45:15 +0300

    Test Firebase GitHub deployment integration

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Sidebar />} />
-        </Routes>
-      </Router>
-      <Feed />
-      <Widget />
+      <h1>hello</h1>
     </div>
   );
 }
